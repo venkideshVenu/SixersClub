4b6d81ad23f37e45b9e7fd81341bb400c0def861
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-09-13 20:39:23 +0530

    Created App Home

--- SixersClub/settings.py
+++ SixersClub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(9j-$d1w5&cyq=(*zm%w1e(ut%e+c@kjot0zq3u7v2-ndpv#k$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
