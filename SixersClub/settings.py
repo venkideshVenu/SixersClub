9baacc744da6227182975f57482b61efc8351296
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-09-22 12:02:42 +0530

    Contact Page email functionality added

--- SixersClub/settings.py
+++ SixersClub/settings.py
@@ -128,3 +128,16 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR/"static"
+
+
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
