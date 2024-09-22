a5b3dc2af8fa5954ab8553f7f202612382430fbf
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-09-23 00:49:54 +0530

    Login template completed

--- SixersClub/urls.py
+++ SixersClub/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('account/', include('account.urls')),
+    path('account/', include('accounts.urls')),
     path('tournaments/', include('tournaments.urls')),
 ]
