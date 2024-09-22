8d247e3284a381505810b6f07739bbd0b47f75ed
Venkidesh Venu <venkidesh.venu.04@gmail.com>
2024-09-22 17:35:13 +0530

    populated tournaments table

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -3,6 +3,6 @@ from .models import Tournament
 
 @admin.register(Tournament)
 class TournamentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'tournament_type', 'start_date', 'end_date', 'registration_deadline', 'registration_fee')
+    list_display = ('name', 'tournament_type', 'start_date', 'end_date', 'registration_deadline', 'registration_fee','photo')
     list_filter = ('tournament_type', 'start_date')
     search_fields = ('name',)
